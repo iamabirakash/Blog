12ab1f1565309dbdded8492bc979e2d952220019
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-25 21:27:17 +0530

    Added Components and updates App.jsx for loading and added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL) for react
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-    <h1>A Blog App</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading? (
+    <div className=''></div>
+  ) : (null)
 }
 
 export default App
